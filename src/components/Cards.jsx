95cb293561bb7010f8189be91e2587b1cd3029f7
Arthur Gonçalves Pacheco <20242INN003@cefetes.br>
2025-09-18 21:10:32 -0300

    Atualizando pasta com novas alterações

---
+++ src/components/Cards.jsx
@@ -0,0 +1,12 @@
+import torre from '../assets/torre.jpg';
+import aviao from '../assets/aviao.jpg';
+
+export default function Cards() {
+    return (
+        <>
+            <img src={torre} alt="Foto da torre" />
+            <img src={aviao} alt="Foto do avião" />
+            <h1>ialackbahhhhh</h1>
+        </>
+    );
+}
