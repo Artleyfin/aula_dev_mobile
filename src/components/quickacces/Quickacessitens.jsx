a0464b32ad4f5a3c70b873bd65b60819cdd993d0
Artleyfin <arthurgpacheco9@gmail.com>
2025-09-25 21:43:39 -0300

    atualização

--- src/components/quickacces/Quickacessitens.jsx
+++ src/components/quickacces/Quickacessitens.jsx
@@ -1,5 +1,7 @@
-export default function Quickaccess({icone}) {
-  return <>
-    <img src={icone} alt="" />
-  </>
+export default function Quickaccess({ icone }) {
+  return (
+    <>
+      <img src={icone} alt="" style={{ width: "16px" }} />
+    </>
+  );
 }
